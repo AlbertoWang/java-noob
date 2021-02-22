d8ff30a6e682073d49a92e02c28e137b89c48b9f
AlbertoWang <albertowang@foxmail.com>
2021-02-22 16:35:54 +0800

    实现一个starter

---
+++ Spring系列代码/springboot-starter/mystarter/src/test/java/cn/albertowang/spring/mystarter/MystarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.albertowang.spring.mystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Spring系列代码/springboot-starter/teststarter/src/test/java/cn/albertowang/teststarter/TeststarterApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.albertowang.teststarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeststarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
